e8a4fb4995de490b536c6580ad9969667d9f7e4d
guibolt <guilhermedosreiss@gmail.com>
2019-11-15 02:51:46 -0300

    final

---
+++ fluxo-e-condicionais/testaBreak.dart
@@ -0,0 +1,11 @@
+void main(){
+  int contador = 0;
+  for(;;){
+    contador++;
+    print("O valor do contador é $contador");
+
+    if(contador == 200)
+    break;
+  }
+     print("Até mais !");
+}
